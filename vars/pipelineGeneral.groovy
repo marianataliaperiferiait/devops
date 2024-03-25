4e716f0d2004546c36bc26b1bf45f3652dd145bd
Natalia Sanchez <m.sanchez@montechelo.online>
2024-03-25 18:24:39 -0500

    Update

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,34 +1,31 @@
-def call (scmUri){
+    def call (scmUri){
 
- pipeline{
-    agent any
-    tools {
-    jdk 'jdk11'
-    maven 'maven3'
-    }
-    stages{
-        stage("Fase 1: Contruccion Applicacion"){
-            steps {
-                script {
-                    def cloneapp = new org.devops.lb_buildartefacto()
-                    cloneapp.clone
-                    def aplicationapp = new org.devops.lb_buildartefacto()
-                    aplicationapp.aplication
-                    def artefactapp = new org.devops.lb_buildartefacto()
-                    artefactapp.artefact                                      
+    pipeline{
+        agent any
+        tools {
+        jdk 'jdk11'
+        maven 'maven3'
+        }
+        stages{
+            stage("Fase 1: Contruccion Applicacion"){
+                steps {
+                    script {
+                        lb_buildartefacto.clone()
+                        lb_buildartefacto.aplication()
+                        lb_buildartefacto.artefact()                                   
+                    }
                 }
-            }
-        }             
-        stage("Fase 2: Scanner Sonnarqube") {
-            steps {
-            script {
-                def analisissonar = new org.devops.lb_analisissonarqube()
-                analisissonar.analisissonar
-                                 
-                 }
-             }
-        }             
+            }             
+            stage("Fase 2: Scanner Sonnarqube") {
+                steps {
+                script {
+                    def analisissonar = new org.devops.lb_analisissonarqube()
+                    analisissonar.analisissonar
+                                    
+                    }
+                }
+            }             
 
-        }                                     
+            }                                     
+        }
     }
- }
