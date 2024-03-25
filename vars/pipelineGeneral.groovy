5caf1977de82523ce0b2e92d718a5625143999ac
marianataliaperiferiait <158316775+marianataliaperiferiait@users.noreply.github.com>
2024-03-25 17:42:20 -0500

    Update pipelineGeneral.groovy

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -2,22 +2,32 @@ def call (){
 
 pipeline{
     agent any
+    tools {
+    jdk 'jdk11'
+    maven 'maven3'
+    }
     stages{
         stage("Fase 1: Contruccion Applicacion"){
             steps {
                 script {
-                    def build cloneapp = new org.devops.lb_buildartefacto()
+                    def cloneapp = new org.devops.lb_buildartefacto()
                     cloneapp.clone
-                    def build aplicationapp = new org.devops.lb_buildartefacto()
-                    aplication.aplication
-                    def build artefactapp = new org.devops.lb_buildartefacto()
+                    def aplicationapp = new org.devops.lb_buildartefacto()
+                    aplicationapp.aplication
+                    def artefactapp = new org.devops.lb_buildartefacto()
                     artefactapp.artefact                                      
                 }
             }
         }             
         stage("Fase 2: Scanner Sonnarqube") {
         }             
-                                   
+            // steps {
+            //     script {
+            //         def analisissonar = new org.devops.lb_analisissonarqube()
+            //         analisissonar .clone
+                                 
+            //     }
+            // }
+        }                                     
     }
  }
-}
