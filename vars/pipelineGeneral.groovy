b0c6675d5b44757a9c0dcd0cce5b152e5c8af155
Natalia Sanchez <m.sanchez@montechelo.online>
2024-03-25 19:34:54 -0500

    Update

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -3,6 +3,11 @@
         agent any
         stages{
             stage("Fase 1: Contruccion Applicacion"){
+                when {
+                    expression {
+                        env.BRANCH_NAME == 'feature'
+                    }
+                }                
                 steps {
                     script {
                         def clone = new org.devops.lb_buildartefacto()
@@ -15,6 +20,11 @@
                 }
             }             
             stage("Fase 2: Scanner Sonnarqube") {
+                when {
+                    expression {
+                        env.BRANCH_NAME == 'feature'
+                    }
+                }                
                 steps {
                 script {
                     def analisissonar = new org.devops.lb_analisissonarqube()
